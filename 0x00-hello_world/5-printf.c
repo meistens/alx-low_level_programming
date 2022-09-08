68ea727226dcbffd6db4336dea0684fcc91d6f4d
Mebo David <mebodave@gmail.com>
2022-09-08 14:30:36 -0700

    added printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using puts to print instead of printf
+ * the program prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
