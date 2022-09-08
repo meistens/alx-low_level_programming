17dd91728fea0292c1866f8c4b09b70c29961a91
Mebo David <mebodave@gmail.com>
2022-09-08 13:56:52 -0700

    modified file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
+/**
+ * main - print the string in the put function
+ *
+ * Description: using puts to print instead of printf
+ * this program prints the  letters "programming is like building a multilingual puzzle"
+ * returns 0
+ */
+
+
+
+
 #include <stdio.h>
 
-int main()
-{ puts ("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
