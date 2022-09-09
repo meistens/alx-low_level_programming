a7a0c0f2e6b73751738dc7348ea9cc615029e876
Mebo David <mebodave@gmail.com>
2022-09-09 13:17:11 -0700

    added new files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Description: prints the lowercase alphabet in reverse
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
