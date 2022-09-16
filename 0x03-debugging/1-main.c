6916b957c34da98db01339711f1e11668f93f84d
Mebo David <mebodave@gmail.com>
2022-09-15 18:58:35 -0700

    it works but sometimes the linter be sus

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,15 @@
 
 int main(void)
 {
-  int i;
-
-  printf("infinite loop incoming :(\n");
-
-  i = 0;
-  
-  /**
-   * while (i < 10)
-   * {
-   * putchar(i);
-   * }
+int i;
+printf("infinite loop incoming :(\n");
+i = 0;  
+/**
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
    */
-
-  printf("infinite loop avoided! \\o/\n");
-
-  return (0);
+printf("infinite loop avoided! \\o/\n");
+return (0);
 }
