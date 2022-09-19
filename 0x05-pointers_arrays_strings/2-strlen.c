8f755a04c6179afcd61721730923876d42ff1ff6
Mebo David <mebodave@gmail.com>
2022-09-19 07:02:14 -0700

    added new file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - get the length of a string
+ * @s: string pointer passed to this function
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
