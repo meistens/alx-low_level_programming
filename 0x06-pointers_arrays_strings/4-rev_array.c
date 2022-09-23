cd0ff15e09728b8ab4abe1ce7daf8d527a28b192
Mebo David <mebodave@gmail.com>
2022-09-23 01:41:34 -0700

    apparently, betty linter cannot tell between a good and bad array format

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse content of array of integer
+ * @a: array
+ * @n: number of the elements of the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+int *p, i, j, k;
+p = a;
+for (i = 0; i < n; i++)
+p++;
+for (k = 0; k < n / 2; k++)
+{
+j = a[k];
+a[k] = *p;
+*p = j;
+p--;
+}
+}
