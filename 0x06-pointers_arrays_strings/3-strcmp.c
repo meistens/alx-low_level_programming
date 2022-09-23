5117c40e6fa63e05deca2579f1c9c037b3d7c6f4
Mebo David <mebodave@gmail.com>
2022-09-23 01:18:28 -0700

    new file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int result;
+result = strcmp(s1, s2);
+return (result);
+}
