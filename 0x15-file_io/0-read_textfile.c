42dbd13360129f07a6d0723eca5422cc4f6e9b1c
David Mebo <mebodave@gmail.com>
2022-11-03 16:07:47 -0700

    README and create file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard
+ * POSIX output
+ * @filename: file to print
+ * @letters: number of letters to print
+ *
+ * Return: number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	char *fd;
+	ssize_t rd, wr;
+
+	if (!filename)
+		return (0);
+
+	fp = open(filename, O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	fd = malloc(sizeof(char) * (letters));
+	if (!fd)
+		return (0);
+
+	rd = read(fp, fd, letters);
+	wr = write(STDOUT_FILENO, fp, rd);
+
+	close(fp);
+
+	free(fd);
+
+	return (wr);
+}
