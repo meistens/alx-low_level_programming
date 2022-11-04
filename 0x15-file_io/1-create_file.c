2176c8e3e4776ceee928760730e470ef8ab6ab14
David Mebo <mebodave@gmail.com>
2022-11-04 05:55:19 -0700

    refactored code to pass checker

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,16 @@ int create_file(const char *filename, char *text_content)
 	int letters;
 	int rwr;
 
+	if (!filename)
+		return (-1);
+
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fp == -1 && !filename)
+	if (fp == -1)
 		return (-1);
+
+	if (!text_content)
+		text_content = "";
 
 	for (letters = 0; text_content[letters]; letters++)
 		;
@@ -24,9 +30,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (rwr == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
 
 	close(fp);
 
