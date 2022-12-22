d8bc76fc82c38d9cabd76979488b25ce0ce5b124
David Mebo <mebodave@gmail.com>
2022-12-22 05:37:16 -0800

    meh

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,29 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to return the square root of.
- * Return: -1 if n does not have a natural square root
+ * _sqrt_recursion - a function that returns
+ * the natural square root of a number.
+ * @n: int n
+ * Return: int
  */
-
 int _sqrt_recursion(int n)
 {
-	/* check if the data (n) is 0 or 1, if so returns the same value */
-	if (n == 0 || n == 1)
-		return (n);
+	return (_sqrt(n, 1));
+}
 
-	/* check if n is negative */
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
 	if (n < 0)
 		return (-1);
-
-	/* recursively call the function here, dividing n by 2 */
-	int i = _sqrt_recursion(n/2);
-
-		/* is square of current number equal to given number? */
+	if ((i * i) > n)
+		return (-1);
 	if (i * i == n)
 		return (i);
-
-	/* but if square of current number is less than given number? */
-	if (i * i < n)
-	{
-		/* do some math, compare em and return either j or -1 */
-		int j = (i + (n/i)) / 2;
-
-		if (j * j == n)
-			return (j);
-		else
-			return (-1);
-	}
-	/* if numbee does not have a natural sqrt, return -1 */
-	return (-1);
+	return (_sqrt(n, i + 1));
 }
